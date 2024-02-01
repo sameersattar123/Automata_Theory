7540eda50505a6804809ad99d330ad836003eb5b
Sameer <sameersattar1111@gmail.com>
2024-02-01 22:56:36 +0500

    lab 01

--- Labs/Lab01/Lab01.py
+++ Labs/Lab01/Lab01.py
@@ -1,1 +1,9 @@
-print("Sameer Sattar")
+import re
+
+def checkStartAndEndString(str):
+    if re.search("^apple.*abc$", str):
+        print("Match Found")
+    else:
+        print("Match Not Found")
+    
+checkStartAndEndString("apple abc") # Match Found
